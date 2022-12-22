c3f33e4fb68a00c1d70fd44fda2292c6d3d13b5c
방지은 <qkdwldms12@naver.com>
2022-12-22 18:29:25 +0900

    12-22 오후 수업

---
+++ src/ch04/home/ex01/Grade.java
@@ -0,0 +1,29 @@
+package ch04.home.ex01;
+
+import java.util.Scanner;
+
+public class Grade {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int score = 0;
+		char grade = 0;
+		System.out.print("점수: "); score = sc.nextInt();
+
+	}
+}
+	
+/*
+다음 구간별로 학점을 부여하라.
+98 이상 A+
+94 이상 A
+90 이상 A-
+88 이상 B+
+84 이상 B
+80 이상 B-
+80 미만 C
+--
+
+점수: 100
+A+
+*/
